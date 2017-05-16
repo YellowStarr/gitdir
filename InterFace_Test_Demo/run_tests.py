a98c1865a168554e6d29b1a93bc2c0380087a907
qiuwj <sillyapplemi@126.com>
2017-05-16 16:11:16 +0800

    ignore pyc 文件。

--- InterFace_Test_Demo/run_tests.py
+++ InterFace_Test_Demo/run_tests.py
@@ -4,24 +4,24 @@ sys.path.append('testcase')
 import HTMLTestRunner
 import unittest
 from testcase import *
-
+import mail
 # test_dir='./testcase'
 # discover=unittest.defaultTestLoader.discover(test_dir,pattern='*_test.py')
 
 if __name__=="__main__":
     print "---------------------------------------start-----------------------------------------"
     caseNames = [
-        loginTests.LoginTest,
-        userinfoTest.userinfoTest,
-        userErrorCheck.userErrorCheck,
-        coreTest.coreTest,
-        coreErrorTest.coreErrorTest,
-        accompany.Accompany,
-        searchTest.SearchTest,
-        mapTest.MapTest,
-        indexTest.IndexTest,
-        shareTest.SearchTest,
-        messageTest.MessageTest,
+        # loginTests.LoginTest,
+        # userinfoTest.userinfoTest,
+        # userErrorCheck.userErrorCheck,
+        # coreTest.coreTest,
+        # coreErrorTest.coreErrorTest,
+        # accompany.Accompany,
+        # searchTest.SearchTest,
+        # mapTest.MapTest,
+        # indexTest.IndexTest,
+        # shareTest.SearchTest,
+        # messageTest.MessageTest,
         indexErrorTest.IndexErrorTest,
 
     ]
@@ -29,9 +29,11 @@ if __name__=="__main__":
     testunit = unittest.TestSuite()
     for i in range(0, len(caseNames)):
         testunit.addTest(unittest.makeSuite(caseNames[i]))
-    nowtime = time.strftime("%Y-%m-%d@%H_%M_%S", time.localtime(time.time()))
-    filename = 'report\\' + nowtime + "reporter.html"
+    # nowtime = time.strftime("%Y-%m-%d@%H_%M_%S", time.localtime(time.time()))
+    filename = 'report\\reporter.html'
     fp = file(filename, 'wb')
     runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='testcase-logintest', description='desc')
 
     runner.run(testunit)
+    m = mail.SendMail()
+    m.send()
